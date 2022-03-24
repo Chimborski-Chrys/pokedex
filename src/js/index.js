5f97e4fbf5ca19ec223c5e7cf9ce00fe03223735
Chrystiomar <chrystiomar@hotmail.com>
2022-03-23 23:45:18 -0300

    ajustes finais no js

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,24 @@
 const listaSelecaoPokemon = document.querySelectorAll('.pokemon')
-const pokemonCard = document.querySelectorAll('.cartao-pokemon')
+//const pokemonCard = document.querySelectorAll('.cartao-pokemon')
 
 listaSelecaoPokemon.forEach(pokemon => {
+
     pokemon.addEventListener('click', () => {
+        
+        const cartaoPokemonAberto = document.querySelector('.aberto')
+        cartaoPokemonAberto.classList.remove('aberto')
+
        const idPokemonSelecionado = pokemon.attributes.id.value
 
-       document.getElementById('cartao-bulbasaur')
+       const idDoCartaoPokemonParaAbri = 'cartao-' + idPokemonSelecionado
+      const cartaoPokemonParaAbrir = document.getElementById(idDoCartaoPokemonParaAbri)
+      cartaoPokemonParaAbrir.classList.add('aberto')
+
+      const pokemonAtivoListagem = document.querySelector('.ativo')
+      pokemonAtivoListagem.classList.remove('ativo')
+
+      const pokmonSelecionadoNaListagem = document.getElementById(idPokemonSelecionado)
+      pokmonSelecionadoNaListagem.classList.add('ativo')
     } )
 })
 
